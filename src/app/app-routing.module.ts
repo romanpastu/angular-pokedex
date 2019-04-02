c6145f44b69ede8f5fad3970792ba9c7d5dfebea
romanpastu <roman.pastushenko.1997@gmail.com>
2019-04-02 13:01:55 +0200

    route guard implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule, Router } from '@angular/router';
 
 import { PokemonListComponent } from '../../src/app/pokemon-list/pokemon-list.component';
 import {LoginComponent} from '../../src/app/login/login.component'
+import { AuthGuard } from './auth.guard';
+
+
 
 const routes: Routes = [
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'pokemonlist', component: PokemonListComponent},
+  {path: 'pokemonlist', component: PokemonListComponent , canActivate: [AuthGuard] },
   {path: 'login', component: LoginComponent},
   { path: '',redirectTo: '/login',pathMatch: 'full'}
 ]
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private afAuth: AngularFireAuth,
+    private myRoute: Router){
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.afAuth.auth.currentUser){
+        return true;
+      }else{
+        this.myRoute.navigate(["login"]);
+        return false;
+      }
+  }
+  
+}
