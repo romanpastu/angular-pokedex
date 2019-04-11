a1cc9c97af74a8b51cef62388efe5c962637da54
romanpastu <roman.pastushenko.1997@gmail.com>
2019-04-11 10:46:32 +0200

    guard

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -69,6 +69,7 @@ export class NavbarComponent implements OnInit {
   }
 
   onUsersClick() {
+    console.log("fist click")
     this.router.navigate(['/userlist']);
     // this.featureSelected.emit(feature);
   }
--- src/app/role.guard.ts
+++ src/app/role.guard.ts
@@ -7,33 +7,38 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
-  data2 
-  constructor(public db: AngularFirestore, private myRoute: Router){
-
-  }
-  canActivate()
-    {
+  data2 //variable to store the role of the current user , (1 or 2) . If its 2 the canActivate should return true.
+  
+  constructor(public db: AngularFirestore, private myRoute: Router) {
+    //role extraction 
+    //We get the role by comparing the uid , and store it in the "data2" var
     var docRef = this.db.collection('users').doc(localStorage.getItem('user'));
-    
-      
+
+
     docRef.get().toPromise().then(doc => {
       if (doc.exists) {
         this.data2 = doc.data().role;
         console.log("data role")
-        console.log(this.data2) 
-        
-      } 
+        console.log(this.data2)
+
+      }
     });
-    if(this.data2 == 2 && localStorage.getItem('user')){
-      console.log("ROLE GUARD")
-      return true;
-    }else{
+
+
+  }
+
+  canActivate() {
+    //if data2 is 2, means that the redirection should work
+    if ( localStorage.getItem('user') && this.data2 == 2 ) {  
+        console.log("ROLE GUARD")
+        return true;
+    } else {
       console.log("ROLE NO GUARD")
       // localStorage.clear();
       return false;
     }
-    
-    
+
+
   }
-  
+
 }
